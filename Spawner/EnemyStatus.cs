3c0f758ed3e9e69d3e8e46be980c9577b3c067dd
bomba14f13 <mborowiec12@gmail.com>
2017-05-14 23:44:30 +0200

    Spawner

---
+++ Spawner/EnemyStatus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatus : Status
+{
+	private int score;
+	
+    public override void onStart()
+    {
+        base.onStart();
+        healthChanged += OnHealthChanged;
+
+        if (GameController.Instance.gameMode)
+            GameController.Instance.gameMode.OnEnemySpawned(this.gameObject);
+    }
+
+    void OnHealthChanged(GameObject obj, float health)
+    {
+        if (health <= 0.0f)
+        {
+			GameObject itemSpawner = GameObject.FindGameObjectsWithTag("itemSpawner")[0];
+			itemSpawner.SendMessage("onEnemyDead", gameObject.transform.position);
+            gameObject.SetActive(false);
+            Destroy(gameObject);			
+        }
+    }
+}
